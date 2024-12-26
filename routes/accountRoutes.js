153025c7d10513ebdc626d4218f8ad586b91ce61
SharkMouth <40568844+SharkMouth@users.noreply.github.com>
2024-12-26 14:10:52 +0700

    Update Account

--- routes/accountRoutes.js
+++ routes/accountRoutes.js
@@ -1,10 +1,10 @@
-// const express = require('express');
-// const { addAccount, login } = require('../controllers/accountController');
+const express = require('express');
+const { addAccount, login } = require('../controllers/accountController');
 
-// const router = express.Router();
+const router = express.Router();
 
-// // Route untuk menambah akun
-// router.post('/register', addAccount);
-// router.post('/login', login)
+// Route untuk menambah akun
+router.post('/register', addAccount);
+router.post('/login', login)
 
-// module.exports = router;
+module.exports = router;
